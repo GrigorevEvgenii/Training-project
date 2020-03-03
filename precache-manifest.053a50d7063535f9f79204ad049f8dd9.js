88c4dfde1970c57eca357fa29ed050d0e27bdd86
Ëvgenii Grigorev <vgenii.grigorev@i3donline.com>
2020-03-03 11:36:10 +0500

    Updates

---
+++ precache-manifest.053a50d7063535f9f79204ad049f8dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcb60d49bf95c4c7fb898cf60bb6c03",
+    "url": "/Training-project/index.html"
+  },
+  {
+    "revision": "62b1cec91cfc9cd82f86",
+    "url": "/Training-project/static/css/main.c5e92e25.chunk.css"
+  },
+  {
+    "revision": "d487a885e6a3ff78a64c",
+    "url": "/Training-project/static/js/2.1956604f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Training-project/static/js/2.1956604f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b1cec91cfc9cd82f86",
+    "url": "/Training-project/static/js/main.933fbad6.chunk.js"
+  },
+  {
+    "revision": "ba24626f15796b4eb3cc",
+    "url": "/Training-project/static/js/runtime-main.792311c5.js"
+  }
+]);
